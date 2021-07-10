592f3ccb017d34fc74086542f1497de191b05718
Eoin Mcloughlin <hkslt@eoinrul.es>
2021-07-10 12:51:02 +0100

    Workaround to bring up keyboard in sounder activity

--- app/src/main/java/es/eoinrul/ecwt/SounderActivity.kt
+++ app/src/main/java/es/eoinrul/ecwt/SounderActivity.kt
@@ -1,15 +1,17 @@
 package es.eoinrul.ecwt
 
-import android.content.ContentProvider
 import android.content.Context
-import android.media.*
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.KeyEvent
-import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceManager
-import es.eoinrul.ecwt.R
+import java.util.*
+
 
 // Activity that only echos any inputs typed on a keyboard
 class SounderActivity : AppCompatActivity() {
@@ -22,22 +24,18 @@ class SounderActivity : AppCompatActivity() {
 
         mTextViewTest = findViewById<TextView>(R.id.keyedText);
 
+        // This is a hidden EditText that's used to bring up the soft keyboard:
+        mKeyboardInput = findViewById<EditText>(R.id.sounderInput);
+        mKeyboardInput?.addTextChangedListener(mInputHandler)
+        mKeyboardInput?.requestFocus();
+
         initSoundPlayer()
-        onKeyUp(KeyEvent.KEYCODE_E, KeyEvent(0,0))
+        onTextEntered("E"); // Display and sound something - this is arbitrary
     }
 
     override fun onResume() {
         super.onResume()
         initSoundPlayer()
-    }
-
-    override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
-        val sequence = KeycodeToSoundSequence(keyCode);
-
-        mTextViewTest?.text = SequenceToString(sequence);
-
-        mSoundPlayer?.enqueue(sequence);
-        return true;
     }
 
     override fun onPause() {
@@ -52,8 +50,42 @@ class SounderActivity : AppCompatActivity() {
         mSoundPlayer = DitDahSoundStream(generatorSettings)
     }
 
+    private fun onTextEntered(typedString : String) {
+        val sequence = StringToSoundSequence(typedString);
+
+        // Display the text:
+        mTextViewTest?.text = SequenceToString(sequence);
+
+        //  Then play the sound:
+        mSoundPlayer?.enqueue(sequence);
+
+        // Finally, clear the input box, as we don't need it anymore:
+        mKeyboardInput?.setText("");
+    }
+
     private var mTextViewTest : TextView? = null;
+    private var mKeyboardInput : EditText? = null;
     private var mMediaPlayer : MediaPlayer? = null;
 
     private var mSoundPlayer : DitDahSoundStream? = null;
+
+    // Utility to watch our EditText and handle any user input:
+    private val mInputHandler = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            if(s == null) {
+                return;
+            }
+
+            if(s.isNotEmpty()) {
+                var typedString: String = s.toString().capitalize();
+                onTextEntered(typedString)
+            }
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+    }
 }
